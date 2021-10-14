c8654df9ff8437acdc63dd22f696d0ef8e19f4e3
ing <ingafter60@outlook.com>
2021-10-14 21:13:15 +0700

    3. Create Lead model

--- apps/leads/admin.py
+++ apps/leads/admin.py
@@ -1,3 +1,11 @@
+# apps/leads/models.py
+
+# Django modules
 from django.contrib import admin
 
+# Locals
+from apps.leads.models import Lead 
+
 # Register your models here.
+
+admin.site.register(Lead)
